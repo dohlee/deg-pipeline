64cc7dbdb02b29de81e7e82f62f4da3d441506cf
dohlee <apap950419@gmail.com>
2018-04-18 00:11:56 +0900

    Start implementing ebseq pipeline.

---
+++ scripts/ebseq.R
@@ -0,0 +1,24 @@
+# Install required packages if they are not installed.
+source("https://bioconductor.org/biocLite.R")
+require('EBSeq') || biocLite('EBseq')
+require('optparse') || install.packages('optparse')
+
+# Parse options.
+option_list = list(
+  make_option(c('-i', '--input'), type='character', default=NULL, help='Input file name.'),
+  make_option(c('-c', '--condition'), type='character', default=NULL, help='Input condition file name.'),
+  make_option(c('-o', '--outdir'), type='character', default='result/ebseq', help='Output directory for EBSeq results. [default=%default]'))
+)
+parser = OptionParser(option_list=option_list)
+args = parse_args(parser)
+
+# Error handling for options.
+if (is.null(args$input)) {
+  print_help(parser)
+  stop("Input file name[-i, --input] should be specified.", call.=FALSE)
+}
+
+if (is.null(args$condition)) {
+  print_help(parser)
+  stop("Condition file name[-c, --condition] should be specified.", call.=FALSE)
+}

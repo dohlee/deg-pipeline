11861ae78764904a708b28d04be8845a29e5fa25
dohlee <apap950419@gmail.com>
2018-04-18 12:32:38 +0900

    Write DEG list to output file.

--- scripts/ebseq.R
+++ scripts/ebseq.R
@@ -1,6 +1,10 @@
 # Install optparse package if it is not installed.
 require('optparse', quietly=TRUE) || install.packages('optparse', repos='http://cran.us.r-project.org')
 
+# Import logger and set the name of the logger.
+source('logging.R')
+set_logger(name='EBSeq')
+
 # Parse options.
 option_list = list(
   make_option(c('-i', '--input'), type='character', default=NULL, help='Input file name.'),
@@ -26,22 +30,32 @@ if (is.null(args$condition)) {
 }
 
 # Get input data and convert it into a matrix.
+flog.info('Parsing input data...')
 input_data = read.table(args$input, row.names=1, header=TRUE, check.names=FALSE)
 data_mat = data.matrix(input_data)
 
 # Parse conditions.
+flog.info('Parsing condition table...')
 conditions = read.table(args$condition, stringsAsFactors=FALSE, row.names=1, header=FALSE)
 
 # Only take samples in intersection.
+flog.info('Taking samples in intersection...')
 common_samples = intersect(colnames(data_mat), rownames(conditions))
 data_mat = data_mat[, common_samples]
 conditions = conditions[common_samples, ]
+flog.info('Total %d samples are going to be analyzed.', length(common_samples))
 
 # Compute size factors.
+flog.info('Computing size factors...')
 size_factors = MedianNorm(data_mat)
 
 # Discover DEGs.
+flog.info('Discovering DEGS...')
+flog.info('Running EBTest...')
 EB_out = EBTest(Data=data_mat, Conditions=as.factor(conditions), sizeFactors=size_factors, maxround=5)
+flog.info('Running GetDEResults...')
 EB_DE_result = GetDEResults(EB_out, FDR=0.05)
 
-print(EB_DE_result$DEfound)
+flog.info('Writing to output file...')
+dir.create(args$outdir, recursive=TRUE, showWarnings=FALSE)
+write.table(EB_DE_result$DEfound, file.path(args$outdir, 'ebseq_degs.txt'), quote=F, row.names=F, col.names=F)
--- scripts/logging.R
+++ scripts/logging.R
@@ -1,8 +1,8 @@
 # Install futile.logger package if it is not installed.
 require('futile.logger', quietly=TRUE) || install.packages('futile.logger', repos='http://cran.us.r-project.org')
 
-set_logger = function(script_name) {
-    # Layout example '[EBSeq|2018-04-18 11:04:44] Running EBSeq...'
-    layout = layout.format(paste('[', script_name, '|~t] ~m'))
+set_logger = function(name) {
+    # Layout example '[EBSeq|2018-04-18 11:04:44] Message'
+    layout = layout.format(paste0('[', name, '|~t] ~m'))
     flog.layout(layout)
 }
